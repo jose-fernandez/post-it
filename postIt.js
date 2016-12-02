357d171546e47381fbb892ebc4e161584f7b4708
jose-fernandez <jose.fernandez91@gmail.com>
2016-12-02 10:07:59 +0100

    in process

--- postIt.js
+++ postIt.js
@@ -27,12 +27,31 @@ class postIt{
 	}
 
 	build(){
-		var postIt=document.createElement("TEXTAREA");
-		postIt.setAttribute("class","post-it");
-		postIt.name=this.id;
-		postIt.cols="20";
-		postIt.rows="9";
-		document.querySelector("div.board").appendChild(postIt);
+	//build the container div
+		this.container=document.createElement("div");
+		this.container.setAttribute("class","post-it");
+		this.container.name=this.id;
+		document.querySelector("div.board").appendChild(this.container);
+	//Create button delete()
+		this.del=document.createElement("img");
+		this.del.setAttribute("src","img/cross.png");
+		document.querySelectorAll("div.post-it")[this.id].appendChild(this.del);
+	//build title input text
+		this.title=document.createElement("input");
+		this.title.setAttribute("file","text");
+		this.title.setAttribute("placeholder","Title");
+		document.querySelectorAll("div.post-it")[this.id].appendChild(this.title);
+	//build the text area
+		this.txt=document.createElement("textarea");
+		this.txt.setAttribute("placeholder","Write your note");
+		this.txt.cols="20";
+		this.txt.rows="9";
+		document.querySelectorAll("div.post-it")[this.id].appendChild(this.txt);
+	//Create the timer
+		this.time=document.createElement("small");
+		this.time.innerHTML="12mins";
+		document.querySelectorAll("div.post-it")[this.id].appendChild(this.time);
+
 	}
 
 }
@@ -40,7 +59,12 @@ class postIt{
 class controler{
 	
 	constructor(){
-		this.postIt=new postIt(2);
+		this.id=0;
+	}
+	createPostIt(){
+		this.id++;
+		//addEventListener(...)
+		this.postIt=new postIt(this.id);
 	}
 }
 
